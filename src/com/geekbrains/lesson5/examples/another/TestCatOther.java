447b95f27d0fdfe13959bc7da5630313f1accf2c
VladimirTitov4 <vtitov4@yandex.ru>
2022-03-21 21:46:57 +0300

    Код 5го урока

---
+++ src/com/geekbrains/lesson5/examples/another/TestCatOther.java
@@ -0,0 +1,12 @@
+package com.geekbrains.lesson5.examples.another;
+
+import com.geekbrains.lesson5.examples.Cat;
+
+public class TestCatOther {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.color = "color";
+//        cat.age = 5;
+    }
+}
